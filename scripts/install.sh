e1bd3c9dce14069d16ba5ea178ef0c058032b271
Ivan Kovtun <jeanlacroix@ukr.net>
2024-07-29 21:41:17 +0200

    Update with Install & Uninstall scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 TOOL_DIR="/usr/local/adbm"
 BIN_DIR="/usr/local/bin"
@@ -18,6 +18,22 @@ if [ ! -d "$TOOL_DIR" ]; then
     echo "Creating tool directory: $TOOL_DIR"
     sudo mkdir -p "$TOOL_DIR"
 fi
+
+for FILE in "${FILES[@]}"; do
+    URL="${REPO}/${BRANCH}/${FILE}"
+    DEST_DIR="$TOOL_DIR/$(dirname "$FILE")"
+
+    if [ ! -d "$DEST_DIR" ]; then
+        sudo mkdir -p "$DEST_DIR"
+    fi
+
+    echo "Downloading $FILE"
+    sudo curl -fsSL "$URL" -o "$TOOL_DIR/$FILE"
+
+    echo "Setting execute permissions for $FILE"
+    sudo chmod +x "$TOOL_DIR/$FILE"
+
+done
 
 echo "Creating symlink in $BIN_DIR"
 sudo ln -sf "$TOOL_DIR/bin/adbm.sh" "$BIN_DIR/adbm"
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
+set -e
+
 TOOL_DIR="/usr/local/adbm"
 BIN_DIR="/usr/local/bin"
+SYMLINK="$BIN_DIR/adbm"
 
-if [ -L "$BIN_DIR/mytool" ]; then
-    echo "Removing symlink from $BIN_DIR"
-    sudo rm "$BIN_DIR/adbm"
+if [ -L "$SYMLINK" ]; then
+    echo "Removing symlink $SYMLINK"
+    sudo rm "$SYMLINK"
+else
+    echo "Symlink $SYMLINK does not exist"
 fi
 
-# Remove tool directory
 if [ -d "$TOOL_DIR" ]; then
     echo "Removing tool directory: $TOOL_DIR"
     sudo rm -rf "$TOOL_DIR"
+else
+    echo "Tool directory $TOOL_DIR does not exist"
 fi
 
 echo "Uninstallation complete."
