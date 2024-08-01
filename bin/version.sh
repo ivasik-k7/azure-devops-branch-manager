0f605eb0422e6afb277a4ecf4e4ea1f091906b60
Ivan Kovtun <jeanlacroix@ukr.net>
2024-08-01 18:04:06 +0200

    Update with reading version metadata

--- bin/version.sh
+++ bin/version.sh
@@ -15,21 +15,15 @@ API_URL="https://api.github.com/repos/$AUTHOR/$REPOSITORY/releases"
 
 get_current_version() {
     if [ -f "$VERSION_FILE" ]; then
-        while IFS= read -r line; do
-            case "$line" in
-            tool:)
-                echo -e "\033[1;34m$line\033[0m"
-                ;;
-            name:* | description:* | version:* | release:* | author:* | license:* | support:*)
-                key=$(echo "$line" | cut -d: -f1 | xargs)
-                value=$(echo "$line" | cut -d: -f2- | xargs)
-                echo -e "  \033[1;32m$key\033[0m: \033[1;37m$value\033[0m" # Green for keys, white for values
-                ;;
-            *)
-                echo "$line"
-                ;;
-            esac
-        done <$VERSION_FILE
+        awk '
+        /^  name:/ {print "Name:", substr($0, index($0, ":") + 2)}
+        /^  description:/ {print "Description:", substr($0, index($0, ":") + 2)}
+        /^  version:/ {print "Version:", substr($0, index($0, ":") + 2)}
+        /^  release:/ {print "Release Date:", substr($0, index($0, ":") + 2)}
+        /^  author:/ {print "Author:", substr($0, index($0, ":") + 2)}
+        /^  license:/ {print "License:", substr($0, index($0, ":") + 2)}
+        /^  support:/ {print "Support Email:", substr($0, index($0, ":") + 2)}
+        ' "$VERSION_FILE"
     else
         echo "Version metadata not found"
         exit 1
@@ -37,11 +31,3 @@ get_current_version() {
 }
 
 get_current_version
-# response=$(curl -s "$API_URL")
-
-# if [ -z "$response" ]; then
-#     echo "No releases found or failed to fetch data."
-#     exit 1
-# fi
-
-# latest_release_version=$(echo "$response" | jq -r '.[0].tag_name')
