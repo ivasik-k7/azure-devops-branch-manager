25f3c525208dfec77c3f999aa948182d95fa39b6
Ivan Kovtun <jeanlacroix@ukr.net>
2024-08-01 18:00:26 +0200

    update version meta reader

--- bin/version.sh
+++ bin/version.sh
@@ -4,22 +4,39 @@
 # echo "version"
 AUTHOR="ivasik-k7"
 REPOSITORY="azure-devops-branch-manager"
-VERSION_FILE="default.conf"
+
+TOOL_DIR="/usr/local/adbm"
+META_FILE="meta.yaml"
+
+VERSION_FILE="$TOOL_DIR/$META_FILE"
 
 # shellcheck disable=SC2034
 API_URL="https://api.github.com/repos/$AUTHOR/$REPOSITORY/releases"
 
-VERSION_FILE="meta.yml"
-
 get_current_version() {
     if [ -f "$VERSION_FILE" ]; then
-        cat "$VERSION_FILE"
+        while IFS= read -r line; do
+            case "$line" in
+            tool:)
+                echo -e "\033[1;34m$line\033[0m"
+                ;;
+            name:* | description:* | version:* | release:* | author:* | license:* | support:*)
+                key=$(echo "$line" | cut -d: -f1 | xargs)
+                value=$(echo "$line" | cut -d: -f2- | xargs)
+                echo -e "  \033[1;32m$key\033[0m: \033[1;37m$value\033[0m" # Green for keys, white for values
+                ;;
+            *)
+                echo "$line"
+                ;;
+            esac
+        done <$VERSION_FILE
     else
-        echo "Version file not found"
+        echo "Version metadata not found"
         exit 1
     fi
 }
 
+get_current_version
 # response=$(curl -s "$API_URL")
 
 # if [ -z "$response" ]; then
